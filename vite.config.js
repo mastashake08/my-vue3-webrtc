f030cb502edb773ca1028dd31853dc4b15a5f147
Hacker God <jyrone.parker@gmail.com>
2025-02-21 18:26:07 -0500

    reponame

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
+const repoName = 'my-vue3-webrtc';
 export default defineConfig({
+  base: `/${repoName}/`, // Ensures correct asset paths
   plugins: [
     vue(),
     VitePWA({
