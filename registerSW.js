f5aaa2aa19f38c3f7c29620c31d7e65b8b8d042a
mastashake08 <jyrone.parker@gmail.com>
2025-02-21 23:26:32 +0000

    Deploying to gh-pages from @ mastashake08/my-vue3-webrtc@f030cb502edb773ca1028dd31853dc4b15a5f147 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/my-vue3-webrtc/sw.js', { scope: '/my-vue3-webrtc/' })})}
